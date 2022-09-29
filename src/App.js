271950554b3a04043bbe0c2d5176cd3a2df1992a
GabrielRM198 <gabrielrochamagalhaes0@gmail.com>
2022-09-28 21:42:00 -0300

    uma leve atualizacao

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import React from'react'
 import Container3 from './Components/Container3'
 import Form from './Components/Form'
 import Footer from './Components/Footer'
+import SubFooter from './Components/SubFooter'
+import styled from 'styled-components'
+
+export const Div = styled.div`
+  
+  width: 100%;
+`
 
 export default function App(){
   return(
-    <>
+    <Div>
       <Container3/>
       <Form/>
       <Footer/>
-    </>
+      <SubFooter/>
+    </Div>
   )
 }
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,17 +1,46 @@
 import React from 'react'
 import styled from 'styled-components'
 
+
 export const Div = styled.div`
     
     background-color: #F2F4F1;
     display: flex;
     width: 100%;
     height: 250px;
+    
 
 `
+export const Nav = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: end;
+    
+
+`
+export const Ul = styled.ul`
+    display: flex;
+    padding: 80px;
+`
+export const Li = styled.li`
+    color: #373737;
+    padding: 30px;
+    list-style: none;
+    cursor: pointer;
+`
+
 
 export default function Footer(){
 return(
-    <Div>coe</Div>
+    <Div>
+        <Nav>
+            <img src="Facebook" alt="" />
+            <Ul>
+                <Li>ABAOUT</Li>
+                <Li>RECIPES</Li>
+                <Li>SUBSCRIBE</Li>
+            </Ul>
+        </Nav>
+    </Div>
 )
 }
